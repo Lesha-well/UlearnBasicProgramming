8bc7d4e39ac77dc800b7643496f373a870783568
Lesha Morozov <morozov.aleksei5960@gmail.com>
2025-10-13 22:47:20 +0500

    pluralize

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		var mod10 = count % 10;
+		var mod100 = count % 100;
+		
+		if (mod10 == 1 && mod100 != 11)
+			return "рубль";
+		if (mod10 >= 2 && mod10 <= 4 && (mod100 < 10 || mod100 >= 20))
+			return "рубля";
+		return "рублей";
+	}
+}

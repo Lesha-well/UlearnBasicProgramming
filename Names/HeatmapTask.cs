4c4a0d62dbf0b2ad0858fd4c3d72a535a7142d58
Lesha Morozov <morozov.aleksei5960@gmail.com>
2025-10-24 13:27:02 +0500

    Names

---
+++ Names/HeatmapTask.cs
@@ -0,0 +1,26 @@
+﻿namespace Names;
+
+internal static class HeatmapTask
+{
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    {
+        var xLabels = new string[30];
+        for (var i = 0; i < xLabels.Length; i++)
+            xLabels[i] = (i + 2).ToString();
+
+        var yLabels = new string[12];
+        for (var i = 0; i < yLabels.Length; i++)
+            yLabels[i] = (i + 1).ToString();
+
+        var heatMap = new double[30, 12];
+        foreach (var name in names)
+            if (name.BirthDate.Day != 1)
+                heatMap[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+
+        return new HeatmapData(
+            "Пример карты интенсивностей",
+            heatMap,
+            xLabels,
+            yLabels);
+    }
+}
---
+++ Names/HistogramTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var labels = new string[31];
+        for (var i = 0; i < labels.Length; i++)
+            labels[i] = (i + 1).ToString();
+        
+        var birthsCounts = new double[31];
+        foreach (var n in names)
+            if (n.Name == name && n.BirthDate.Day != 1)
+                birthsCounts[n.BirthDate.Day - 1]++;
+
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'",
+            labels,
+            birthsCounts);
+    }
+}
